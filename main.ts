de2560cf455a1a1cccf18b0c6db81cf8528d7cd7
loganviveiros <72992086+loganviveiros@users.noreply.github.com>
2020-10-21 14:07:43 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+scene.setBackgroundColor(12)
+let Romeo = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f e e e e f f . . . . 
+    . . . f e e e f f e e e f . . . 
+    . . f f f f f 2 2 f f f f f . . 
+    . . f f e 2 e 2 2 e 2 e f f . . 
+    . . f e 2 f 2 f f 2 f 2 e f . . 
+    . . f f f 2 2 e e 2 2 f f f . . 
+    . f f e f 2 f e e f 2 f e f f . 
+    . f e e f f e e e e f e e e f . 
+    . . f e e e e e e e e e e f . . 
+    . . . f e e e e e e e e f . . . 
+    . . e 4 f f f f f f f f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+Romeo.setPosition(80, 104)
+let Juliet = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+Juliet.setPosition(80, 39)
+Juliet.say("Romeo", 2000)
+pause(2000)
+Romeo.say("My dear?", 2000)
+pause(5000)
+Juliet.say("At what o, clock to-morrow shall I send to thee", 5000)
+pause(2000)
+Romeo.say("At the hour of nine", 5000)
